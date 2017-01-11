738198ed99d4a412681cca819f0b0fbd2b1d1300
vvicente <vvicente@deloitte.pt>
2017-01-11 11:51:49 +0000

    VSProject first commit to git

---
+++ AnotherMVC/AnotherMVC/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AnotherMVC.Startup))]
+namespace AnotherMVC
+{
+
+    /*
+     * 
+     * A comment introduced for review purposes only... i do this quite a lot!
+     * 
+     */
+     
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
